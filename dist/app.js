e874a36dbca81de17c393f6e1638b31c615ebe45
sly <slydevwebproject@outlook.fr>
2022-07-22 17:30:45 +0200

    config ts presque fini

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,7 @@
-var counter = document.querySelector('#counter');
-var i = 0;
-var increment = function (e) {
+const counter = document.querySelector('#counter');
+let i = 0;
+const increment = (e) => {
     i++;
-    counter.querySelector('span').innerText = i;
+    counter.querySelector('span').innerText = i.toString();
 };
 counter.addEventListener('click', increment);

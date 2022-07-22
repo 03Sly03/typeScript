19c0b15cd3565889ec1a09694ce2ea95a94c2d2b
sly <slydevwebproject@outlook.fr>
2022-07-22 18:07:01 +0200

    configuration de tsconfig.json strict = true, ajout d'exemple de constantes sur app.ts

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,16 @@
-const counter = document.querySelector('#counter');
-let i = 0;
-const increment = (e) => {
-    i++;
-    counter.querySelector('span').innerText = i.toString();
-};
-counter.addEventListener('click', increment);
+"use strict";
+const a = "Hello World!";
+const numb = 3;
+const b = true;
+const n = null;
+const arr = ['str', 'ing', 'char'];
+const user = { firstname: 'Sly', lastname: 'Man', job: 'couvreur' };
+const user2 = { username: 'Sloum' };
+const date = new Date();
+// const counter = document.querySelector('#counter');
+// let i = 0;
+// const increment = (e) => {
+//     i++;
+//     counter.querySelector('span').innerText = i.toString();
+// }
+// counter.addEventListener('click', increment);
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
-const counter = document.querySelector('#counter');
-let i = 0;
+const a: string = "Hello World!";
+const numb: number = 3;
+const b: boolean = true;
+const n: null = null;
+const arr: string[] = ['str', 'ing', 'char'];
+const user: {[key: string]: string, firstname: string, lastname: string} = {firstname: 'Sly', lastname: 'Man', job: 'couvreur'};
+const user2: {username: string, age?: number} = {username: 'Sloum'};
+const date: Date = new Date();
 
-const increment = (e) => {
-    i++;
-    counter.querySelector('span').innerText = i.toString();
-}
+// const counter = document.querySelector('#counter');
+// let i = 0;
 
-counter.addEventListener('click', increment);
+// const increment = (e) => {
+//     i++;
+//     counter.querySelector('span').innerText = i.toString();
+// }
+
+// counter.addEventListener('click', increment);
